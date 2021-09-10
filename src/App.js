ce2b140353c6f63dcb45952f1d9da60bbbcd4f42
Saurabh Gupta <saurabhg@uvxcel.com>
2021-09-10 23:18:14 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
 import './App.css';
-
+const api = {
+  key: "88a27d70ae10bfa43afa44e64fcd9c4c",
+  base: "https://api.openweathermap.org/data/2.5/"
+}
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input type="text" className="search-bar" placeholder="search..." />     </div>
+      </main>
     </div>
   );
 }
